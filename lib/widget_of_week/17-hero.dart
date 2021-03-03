51afc46381b909092a2358243a80d23ef187be5b
Avi Kathuria <avikathuria21@gmail.com>
2021-03-03 09:44:15 +0530

    Cleanup #2

--- lib/widget_of_week/17-hero.dart
+++ lib/widget_of_week/17-hero.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_practise/widget_of_week/constants.dart';
 
 //hero
 //https://www.youtube.com/watch?v=Be9UH1kXFDw&list=PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG&index=18&ab_channel=GoogleDevelopers
-
 
 class Widget17 extends StatelessWidget {
   @override
@@ -17,7 +17,8 @@ class Widget17 extends StatelessWidget {
         alignment: Alignment.bottomCenter,
         child: InkWell(
           onTap: () {
-            Navigator.pushNamed(context, '/hero_detail');
+            Navigator.pushNamed(
+                context, Constants.Initial_Route + 'hero_detail');
           },
           child: ClipOval(
             child: Hero(
